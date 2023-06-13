2d64c140ffd9f14a5dd16f70bc1f45e84a0c5241
Skirall <krzysztof.zdonek@edu.uekat.pl>
2023-06-13 13:32:53 +0200

    Mapa, chyba działa

--- app/src/main/java/com/example/java_aplikacja/PokazSilownieActivity.java
+++ app/src/main/java/com/example/java_aplikacja/PokazSilownieActivity.java
@@ -1,14 +1,26 @@
 package com.example.java_aplikacja;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PokazSilownieActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class PokazSilownieActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokaz_silownie);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
