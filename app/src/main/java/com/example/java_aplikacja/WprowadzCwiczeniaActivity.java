edc0bed30aa40a35db2c83c0128d1e50f44d39db
Skirall <krzysztof.zdonek@edu.uekat.pl>
2023-06-14 13:02:16 +0200

    Liczenie kalorii

--- app/src/main/java/com/example/java_aplikacja/WprowadzCwiczeniaActivity.java
+++ app/src/main/java/com/example/java_aplikacja/WprowadzCwiczeniaActivity.java
@@ -11,4 +11,5 @@ public class WprowadzCwiczeniaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wprowadz_cwiczenia);
     }
+    
 }
